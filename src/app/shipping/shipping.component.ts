d45a91039fefdaa5d4ec7fbfc890afe6d1bc93ba
elliot-wilson <elliotandrewwilson@gmail.com>
2021-12-16 20:37:38 -0500

    update shipping component HTML

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -8,7 +8,7 @@ import { CartService } from '../cart.service';
 })
 export class ShippingComponent implements OnInit {
   shippingCosts = this.cartService.getShippingPrices();
-  
+
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {}
